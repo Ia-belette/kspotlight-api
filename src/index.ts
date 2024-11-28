2ee46e45a17d53c7769f89bcabb57f1c596f806b
dereje <getutadessedereje@icloud.com>
2024-11-28 20:28:57 +0100

    feat(api): add API key authorization middleware for secure access

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
 import content from "./routes/content";
 import category from "./routes/category";
 
@@ -6,16 +7,27 @@ export type Bindings = {
   XATA_BRANCH: string;
   XATA_API_KEY: string;
   TMDB_API_KEY: string;
+  KSPOTLIGHT_API_KEY: string;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+app.use("*", async (c, next) => {
+  const apiKey = c.req.header("Authorization");
+
+  if (!apiKey) {
+    throw new HTTPException(401, { message: "Missing API Key" });
+  }
+
+  const validApiKey = c.env.KSPOTLIGHT_API_KEY;
+  if (apiKey !== validApiKey) {
+    throw new HTTPException(403, { message: "Invalid API Key" });
+  }
+
+  await next();
 });
 
 app.route("/content", content);
 app.route("/category", category);
-
 
 export default app;
