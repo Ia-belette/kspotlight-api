964ea4fb48b9365e0c9b1e8c67baff3d7904e67b
dereje <getutadessedereje@icloud.com>
2024-11-29 12:44:59 +0100

    refactor: update Swagger UI title and use import aliases

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { swaggerUI } from '@hono/swagger-ui';
+import { SwaggerUI, swaggerUI } from '@hono/swagger-ui';
 import { OpenAPIHono } from '@hono/zod-openapi';
 import { HTTPException } from 'hono/http-exception';
 
-import category from './routes/category';
-import content from './routes/content';
+import category from '@/routes/category';
+import content from '@/routes/content';
 
 export type Bindings = {
   XATA_BRANCH: string;
@@ -24,12 +24,32 @@ app.openAPIRegistry.registerComponent(
   }
 );
 
-app.get('/ui', swaggerUI({ url: '/doc' }));
+app.get('/', (c) => {
+  return c.html(`
+    <html lang="en">
+      <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="KSpotlight" />
+        <title>KSpotlight</title>
+        <script>
+        </script>
+        <style>
+          body {
+            background: #F1FAFD;
+          }
+        </style>
+      </head>
+      ${SwaggerUI({ url: '/doc' })}
+    </html>
+  `);
+});
+
 app.doc('/doc', {
   openapi: '3.0.0',
   info: {
     version: '1.0.0',
-    title: 'My API',
+    title: 'KSpotlight API',
   },
   security: [
     {
