16bd48885bbe7fb71187be0ff408deea08c64c5a
dereje <getutadessedereje@icloud.com>
2024-11-29 17:49:17 +0100

    feat(create-content): add  method for content creation - Added logic to fetch content details from TMDb API. - Integrated genre handling and category creation if needed. - Ensures duplicate content is not added to the database. - Returns newly created content or existing content if already present.

--- src/services/content.ts
+++ src/services/content.ts
@@ -4,6 +4,8 @@ import { validateCursor, validateId, validatePageSize } from '@/utils';
 
 import { ContentServiceProtocol } from '@/interfaces/content';
 import { ContentRecord, XataClient } from '@/xata';
+import { TmdbService } from './tmdb';
+import { HTTPException } from 'hono/http-exception';
 
 export class ContentServices implements ContentServiceProtocol {
   private xata: XataClient;
@@ -15,11 +17,7 @@ export class ContentServices implements ContentServiceProtocol {
     this.xata = new XataClient({ apiKey, branch });
   }
 
-  async searchContents(
-    query: string,
-    pageSize: number = 20,
-    after?: string
-  ): Promise<Page<ContentRecord>> {
+  async searchContents(query: string, pageSize: number = 20, after?: string) {
     pageSize = validatePageSize(pageSize);
     after = validateCursor(after);
 
@@ -91,5 +89,92 @@ export class ContentServices implements ContentServiceProtocol {
       .getPaginated({
         pagination: { size: 8 },
       });
+  }
+
+  async addContent(
+    tmdbId: string,
+    type: string,
+    recommended: boolean,
+    tmdbApiKey: string
+  ) {
+    try {
+      const tmdbApi = new TmdbService(tmdbApiKey);
+
+      const { providers, contentDetails } = await tmdbApi.fetchContentInfo(
+        tmdbId,
+        type
+      );
+
+      const {
+        genres,
+        release_date,
+        backdrop_path,
+        original_title,
+        poster_path,
+        title,
+        tagline,
+        overview,
+        vote_average,
+      } = contentDetails || {};
+
+      const existingContent = await this.xata.db.content
+        .filter({ content_id: parseInt(tmdbId) })
+        .getFirst();
+
+      if (existingContent) {
+        throw new HTTPException(400, { message: 'Content already exists' });
+      }
+
+      let newContent;
+
+      if (!existingContent) {
+        let category = null;
+        if (genres && genres.length > 0) {
+          const mainGenre = genres[0];
+          let existingCategory = await this.xata.db.categories
+            .filter({ category_id: mainGenre.id })
+            .getFirst();
+
+          if (!existingCategory) {
+            existingCategory = await this.xata.db.categories.create({
+              category_id: mainGenre.id,
+              name: mainGenre.name,
+            });
+          }
+
+          category = existingCategory.id;
+        }
+        newContent = await this.xata.db.content.create({
+          content_id: parseInt(tmdbId),
+          title: title || original_title || 'Titre inconnu',
+          original_title: original_title || 'Titre original inconnu',
+          tagline: tagline || '',
+          release_date: release_date ? new Date(release_date) : null,
+          poster_url: poster_path
+            ? `https://image.tmdb.org/t/p/original${poster_path}`
+            : null,
+          backdrop_url: backdrop_path
+            ? `https://image.tmdb.org/t/p/original${backdrop_path}`
+            : null,
+          type: 'movie',
+          category: category,
+          overview: overview,
+          vote_average: String(vote_average),
+        });
+ 
+      } else {
+        newContent = existingContent;
+      }
+
+      return {
+        message: 'Content added successfully',
+        content: newContent,
+      };
+    } catch (error: any) {
+      throw new HTTPException(500, {
+        message: 'Failed to add content',
+        cause: error.message,
+      });
+    }
   }
 }
